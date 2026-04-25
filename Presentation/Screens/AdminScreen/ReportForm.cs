9f3fbcae8260da8483b0361a717a59c91f6d111f
Phượng Trần <pt8803974@gmail.com>
2026-04-25 19:32:39 +0700

    error

---
+++ Presentation/Screens/AdminScreen/ReportForm.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+
+using System.Windows.Forms;
+using Presentation;
+
+namespace Presentation.Screens.AdminScreen
+{
+    public partial class ReportForm : BaseForm
+    {
+        public ReportForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Presentation/Screens/AdminScreen/UserManagementForm.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+namespace Presentation.Screens.AdminScreen
+{
+    public partial class UserManagementForm : BaseForm
+    {
+        public UserManagementForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
