fee3ad3e39199141a6b2162720157e1616687fb6
Phượng Trần <phuongtran.31241022663@st.ueh.edu.vn>
2026-04-25 19:49:15 +0700

    fix 3

---
+++ Application/Services/MapService.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Application.Interfaces;
+using Domain.ValueObjects;
+using Infrastructure.Interfaces; // IGMapService
+
+namespace Infrastructure.ExternalServices
+{
+    public class MapService : IMapService
+    {
+        private readonly IGMapService _gMapService;
+
+        public MapService(IGMapService gMapService)
+        {
+            _gMapService = gMapService;
+        }
+
+        public async Task<Route> GetRouteAsync(Location pickup, Location destination)
+        {
+            return await _gMapService.GetRouteAsync(pickup, destination);
+        }
+
+        public async Task<List<Location>> SearchLocationAsync(string query)
+        {
+            return await _gMapService.SearchLocationAsync(query);
+        }
+
+        public async Task<Location> ReverseGeocodeAsync(double latitude, double longitude)
+        {
+            return await _gMapService.ReverseGeocodeAsync(latitude, longitude);
+        }
+    }
+}
