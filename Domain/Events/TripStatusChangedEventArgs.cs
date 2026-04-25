77b35ad91b45dd8d84f6c3dd9edc015e983b4f77
Phượng Trần <phuongtran.31241022663@st.ueh.edu.vn>
2026-04-25 19:42:41 +0700

    fix 1

---
+++ Domain/Events/TripStatusChangedEventArgs.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Domain.Enums;
+
+namespace Application.Events
+{
+    public class TripStatusChangedEventArgs : EventArgs
+    {
+        public Guid TripId { get; }
+        public TripStatus NewStatus { get; }
+        public Guid? DriverId { get; }
+
+        public TripStatusChangedEventArgs(Guid tripId, TripStatus newStatus, Guid? driverId)
+        {
+            TripId = tripId;
+            NewStatus = newStatus;
+            DriverId = driverId;
+        }
+    }
+}
