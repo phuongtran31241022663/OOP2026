bfaabaf6dbf3ea6555b6a7308ffa221de1e0be9f
Phượng Trần <pt8803974@gmail.com>
2026-04-25 19:49:15 +0700

    fix 3

---
+++ Application/Interfaces/IMatchingService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Application.Interfaces
+{
+    public interface IMatchingService
+    {
+        /// <summary>
+        /// Ghép một tài xế với chuyến đi.
+        /// </summary>
+        /// <param name="tripId">ID của chuyến đi</param>
+        /// <param name="driverId">ID của tài xế</param>
+        /// <returns>True nếu ghép thành công, false nếu không thể ghép</returns>
+        Task<bool> MatchDriverToTripAsync(Guid tripId, Guid driverId);
+    }
+}
